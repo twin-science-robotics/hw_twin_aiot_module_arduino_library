4e42846ebd45a34e144462caeca49ca5b2d755d4
abdullahtemel97 <abdullahtemel.9797@gmail.com>
2025-08-11 18:37:34 +0300

    added examples

---
+++ examples/Blink_RGB/Blink_RGB.ino
@@ -0,0 +1,27 @@
+#include <FastLED.h>
+
+#define RGB_PIN 15  
+#define NUM_LEDS 1
+CRGB leds[NUM_LEDS];
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+FastLED.addLeds<WS2812B,RGB_PIN , RGB>(leds, 1);
+  FastLED.setBrightness(255);
+  leds[0] = CRGB(255,0,0);
+  FastLED.show();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  leds[0] = CRGB(255,0,0);
+  FastLED.show();
+  delay(1000);
+  leds[0] = CRGB(0,0,0);
+  FastLED.show();
+  delay(1000);
+}
